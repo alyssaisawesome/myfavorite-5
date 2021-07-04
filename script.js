29c6ca3b862b69ecac63c62ebc16fdf5baee7d0d
alyssaisawesome <86136406+alyssaisawesome@users.noreply.github.com>
2021-07-04 14:39:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+function imagechange() {
+  document.getElementById("image1").src="https://pyxis.nymag.com/v1/imgs/1cc/b68/6613f73d72328f430c6e55e3de5d3acccf-24-parenting-books-1.2x.rsocial.w600.jpg"
+}
